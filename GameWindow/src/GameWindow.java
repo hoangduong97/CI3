70fcc84a3eb58816d0370f78919c718791f8e593
hoangduong97 <hoang_duong_1997@yahoo.com>
2016-02-29 20:39:13 +0700

    update

--- GameWindow/src/GameWindow.java
+++ GameWindow/src/GameWindow.java
@@ -9,13 +9,47 @@ import java.io.*;
  * Created by Hoang Duong on 27/02/2016.
  */
 public class GameWindow extends Frame implements KeyListener, Runnable, MouseMotionListener {
+    //start
+    Graphics seconds;
+    Image image;
+
+    @Override
+    public void update(Graphics g) {
+        if (image == null) {
+            image = createImage(this.getWidth(), this.getHeight());
+            seconds = image.getGraphics();
+        }
+        seconds.setColor(getBackground());
+        seconds.fillRect(0, 0, getWidth(), getHeight());
+        seconds.setColor(getForeground());
+        paint(seconds);
+        g.drawImage(image, 0, 0, null);
+    }
+
+    //end
     BufferedImage backGround;
-    BufferedImage plane1;
-    int planeX = 150;
-    int planeY = 550;
-    int direction = 0;
+    Plane plane1;
+    Plane plane2;
 
     public GameWindow() {
+        /* Hide mouse cursor */
+        setCursor(Toolkit.getDefaultToolkit().createCustomCursor(new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB), new Point(0, 0), "cursor"));
+        /* */
+
+        plane1 = new Plane();
+        plane1.setPositionX(150);
+        plane1.setPositionY(300);
+        plane1.setSpeed(3);
+        plane1.setDirection(0);
+        plane1.setType(1);
+
+        plane2 = new Plane();
+        plane2.setPositionX(150);
+        plane2.setPositionY(150);
+        plane2.setSpeed(5);
+        plane2.setDirection(0);
+        plane2.setType(2);
+
         this.setTitle("Game 1945");
         this.setSize(400, 640);
         this.setVisible(true);
@@ -31,7 +65,11 @@ public class GameWindow extends Frame implements KeyListener, Runnable, MouseMot
         } catch (IOException e) {
         }
         try {
-            plane1 = ImageIO.read(new File("Resouces/PLANE1.png"));
+            plane1.setSprite(ImageIO.read(new File("Resouces/plane1.png")));
+        } catch (IOException e) {
+        }
+        try {
+            plane2.setSprite(ImageIO.read(new File("Resouces/PLANE2.png")));
         } catch (IOException e) {
         }
         this.addKeyListener(this);
@@ -42,7 +80,8 @@ public class GameWindow extends Frame implements KeyListener, Runnable, MouseMot
     @Override
     public void paint(Graphics g) {
         g.drawImage(backGround, 0, 0, null);
-        g.drawImage(plane1, planeX, planeY, null);
+        plane1.draw(g);
+        plane2.draw(g);
     }
 
     @Override
@@ -53,34 +92,27 @@ public class GameWindow extends Frame implements KeyListener, Runnable, MouseMot
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyChar() == 'w' || e.getKeyChar() == 'W') {
-            direction = 1;
+            plane1.setDirection(1);
         } else if (e.getKeyChar() == 's' || e.getKeyChar() == 'S') {
-            direction = 2;
+            plane1.setDirection(2);
         } else if (e.getKeyChar() == 'a' || e.getKeyChar() == 'A') {
-            direction = 3;
+            plane1.setDirection(3);
         } else if (e.getKeyChar() == 'd' || e.getKeyChar() == 'D') {
-            direction = 4;
+            plane1.setDirection(4);
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        direction = 0;
+        plane1.setDirection(0);
     }
 
     @Override
     public void run() {
         int x = 0;
         while (true) {
-            if (direction == 1) {
-                planeY -= 5;
-            } else if (direction == 2) {
-                planeY += 5;
-            } else if (direction == 3) {
-                planeX -= 5;
-            } else if (direction == 4) {
-                planeX += 5;
-            }
+            plane1.update();
+            plane2.update();
             repaint();
             try {
                 Thread.sleep(17);
@@ -97,7 +129,7 @@ public class GameWindow extends Frame implements KeyListener, Runnable, MouseMot
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        planeX = e.getX();
-        planeY = e.getY();
+        plane2.setPositionX(e.getX() - plane2.getSprite().getWidth() / 2);
+        plane2.setPositionY(e.getY());
     }
 }
--- GameWindow/src/Main.java
+++ GameWindow/src/Main.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
-
 /**
  * Created by Hoang Duong on 27/02/2016.
  */
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         GameWindow game = new GameWindow();
         Thread thread = new Thread(game);
         thread.start();
     }
 }
+
+
+/*
+    - Hidden cursor
+    - Sua class Plane: 1 plane chay = chuot, 1 plane chay = phim
+    - Xem lai tat ca thuoc tinh, dat access modifier tuong ung (public, private) (them getter, setter)
+    - Ban dan
+ */
